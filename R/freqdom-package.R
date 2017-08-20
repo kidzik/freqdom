e2031746546ca784005cda1d6e35941ab8178e88
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2017-08-19 22:07:31 -0700

    check() with no error or notes

--- R/freqdom-package.R
+++ R/freqdom-package.R
@@ -11,27 +11,17 @@
 #' a multivariate time series into to uncorrelated components. Compared to
 #' classical PCA, these components are uncorrelated for any lag. For details refer to
 #' functions
-#' \code{\link{dprcomp}} for estimating the components, \code{\link{dpca.scores}} for estimating
-#' scores and \code{\link{dpca.inverse}} for retrieving the signal from components,
-#' * Multivariate linear regression model where both independent variables (regressors, features)
-#' and dependent variables (response, target, outcome) are multivariate time series.
-#' In this case we developed two approaches:
-#'     1. If regression coefficients \eqn{A_0} depend only on one observation, i.e.
-#'     \deqn{Y_t = A_0 X_t + \varepsilon_t,}
-#'     then refer to function \code{\link{reg.est}}.
-#'     2. If regression depends also on future and past, i.e. there are \eqn{A_k \neq 0} for \eqn{k \neq 0} and
-#'     \deqn{Y_t = \sum_{k=-q}^p A_k X_{t-k} + \varepsilon_t}
-#'     then refer to function \code{\link{lagreg.est}}.
+#' \code{\link{dpca}} for estimating the components, \code{\link{dpca.scores}} for estimating
+#' scores and \code{\link{dpca.KLexpansion}} for retrieving the signal from components,
 #'
 #' Apart from implementing frequency domain techniques for stationary multivariate time series,
 #' \pkg{freqdom} provides a toolset of operators such as the multivariate Fourier Transform
 #' (\code{\link{fourier.transform}}) or a multivariate spectral density operator
 #' (\code{\link{spectral.density}}) as well as simulation of popular multivariate time series
-#' models \code{\link{rar}}, \code{\link{rma}}, \code{\link{rbm}} generating multivariate
-#' autoregressive, moving average and Brounian motion respectively.
+#' models \code{\link{rar}}, \code{\link{rma}} generating multivariate
+#' autoregressive and moving average respectively.
 #' These functions enable developing new techniques based on the Frequency domain analysis.
 #'
-#' @author Siegfried Hormann and Lukasz Kidzinski
 #' @references Hormann Siegfried, Kidzinski Lukasz and Hallin Marc.
 #' \emph{Dynamic functional principal components.} Journal of the Royal
 #' Statistical Society: Series B (Statistical Methodology) 77.2 (2015): 319-348.
@@ -41,4 +31,5 @@
 #' @references Hormann Siegfried and Kidzinski Lukasz.
 #' \emph{A note on estimation in Hilbertian linear models.}
 #' Scandinavian journal of statistics 42.1 (2015): 43-62.
+#' @aliases freqdom-package
 "_PACKAGE"
--- tests/dpca.R
+++ tests/dpca.R
@@ -1,12 +1,17 @@
 library("freqdom")
-Psi = 3:1 %*% t(3:1) / 20
-X = rar(100,3,Psi)
-Ndpc = 1
-q = 30
+library("astsa")
+Y = scale(lap)
 
-res = list()
-res$spec.density = spectral.density(X)
-res$filters = dpca.filters(res$spec.density, q = q, Ndpc = Ndpc)
-res$scores = dpca.scores(X, res$filters)
-res$var = dpca.var(res$spec.density)
-res$Xhat = dpca.KLexpansion(X, res$filters)
+# Compute DPCA with only one component
+res.dpca = dpca(Y, q = 5, Ndpc = 1)
+
+# Compute PCA with only one component
+res.pca = prcomp(Y,center = TRUE)
+res.pca$x[,-1] = 0
+
+# Reconstruct the data
+var.dpca = (1 - sum((res.dpca$Xhat - Y)**2) / sum(Y**2))*100
+var.pca = (1 - sum( (res.pca$x %*% t(res.pca$rotation) - Y)**2) / sum( (Y)**2))*100
+
+cat("Variance explained by DPCA:\t",var.dpca,"%\n")
+cat("Variance explained by PCA:\t",var.pca,"%\n")
